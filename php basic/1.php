2b485b2b76c68bfb11669b6df3361482e90ec294
Nittaya <nittayasairat18@gmail.com>
2024-09-12 15:27:00 +0700

    Add files via upload

---
+++ php basic/1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello PHP</title>
+</head>
+<body>
+    <?php
+        echo "Hello PHP ";
+        echo "<br>";
+        echo "this is my first time to write PHP Langua";
+        define("MINSIZE",50);
+    ?>
+</body>
+</html>
---
+++ php basic/function26.php
@@ -0,0 +1,22 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WriteMesses</title>
+ </head>
+ <body>
+    <?php 
+      function writeMesses() {
+         //create function
+         //start function
+         echo "Yon are really a nice person, Have a nice time!";
+         echo "Have a nice program";
+      }
+         //stop function
+
+      //call function   
+      writeMesses();
+    ?>
+ </body>
+ </html>
